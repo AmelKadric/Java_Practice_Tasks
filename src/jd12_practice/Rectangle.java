e31a2a23fe0fabcf1f2da07308186c8e5eb27912
amelkadric <amelkadric1990@gmail.com>
2024-02-19 10:38:08 -0600

    adding new class (Rectangle)

--- src/jd12_practice/Rectangle.java
+++ src/jd12_practice/Rectangle.java
@@ -1,75 +1,54 @@
 package jd12_practice;
 
 public class Rectangle {
-
-    private double length;
     private double width;
+    private double length;
 
-    public double getLength(){
-        if (length <= 0){
-            System.err.println("Invalid");
-            System.exit(1);
-        }
-        return length;
+    public Rectangle(double width, double length) {
+        setWidth(width);
+        setLength(length);
     }
 
-    public double getWidth(){
-        if (width <= 0){
-            System.err.println("Invalid");
-            System.exit(1);
-        }
+    public double getWidth() {
         return width;
     }
 
-    public void setLength(double length){
-        this.length = length;
-    }
-
-    public void setWidth(double width){
+    public void setWidth(double width) {
+        if (width <= 0) {
+            System.err.println("Width must be a positive value.");
+            System.exit(1);
+        }
         this.width = width;
     }
 
-    public Rectangle(double length, double width) {
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if (length <= 0) {
+            System.err.println("Length must be a positive value.");
+            System.exit(1);
+        }
         this.length = length;
-        this.width = width;
     }
 
-    public double calArea(){
-        return length*width;
+    public double calcArea() {
+        return width * length;
     }
 
-    public double calPerimeter(){
-        return 2*(length+width);
+    public double calcPerimeter() {
+        return 2 * (width + length);
     }
 
     @Override
     public String toString() {
         return "Rectangle{" +
-                "length=" + length +
-                ", width=" + width +
+                "width=" + width +
+                ", length=" + length +
+                ", area=" + calcArea() +
+                ", perimeter=" + calcPerimeter() +
                 '}';
     }
 }
 
-/*
-2. Create a custom class named Rectangle with the following specifications:
-
-	Attributes:
-		width
-		length
-
-	Ensure encapsulation for both fields.
-		Conditions:
-			The width and length cannot be set to negative or zero values.
-
-	Add a constructor that allows the user to set both fields during object creation.
-
-	Actions:
-		calcArea(): Calculates and returns the area of the rectangle object.
-		calcPerimeter(): Calculates and returns the perimeter of the rectangle object.
-		toString(): Displays the width, length, area, and perimeter of the rectangle
-		 when the object is printed.
-
-	Create another class named RectangleClients, create multiple rectangle objects,
-	 and test each function of the rectangle object.
- */
